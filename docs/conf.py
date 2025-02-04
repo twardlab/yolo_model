52fb9d4c67b99243c724c77a087d9510ae101218
Andrew Bennecke <jandrew624@hotmail.com>
2025-02-04 14:38:44 -0800

    Consolidate files

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,17 @@ author = 'Andrew Bennecke, Daniel Tward'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinxcontrib.htmlhelp'
+]
+
+# Configuration for LaTeX output
+latex_documents = [
+    ('index', 'yolo_help.tex', project, author, 'manual'),
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -27,5 +37,5 @@ sys.path.append('/home/runner/work/yolo_model/yolo_model/source')
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
